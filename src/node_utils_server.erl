a8b4e9ab438900d32834d346988fdec445757c8a
alex2308 <alexandre.carlier@student.uclouvain.be>
2018-08-07 01:15:01 +0200

    ...

--- src/node_utils_server.erl
+++ src/node_utils_server.erl
@@ -39,7 +39,7 @@ init([]) ->
 	State = #samples_state{s1 = S1},
 	GCInterval = node_config:get(gc_interval, ?MIN),
     erlang:send_after(5000, self(), {get_cpu_usage}),
-    erlang:send_after(GCInterval, self(), {gc, GCInterval}),
+    %erlang:send_after(GCInterval, self(), {gc, GCInterval}),
 		% {ok, State, 5000}.
 	{ok, State}.
 
