39970d0415718b3c3f502e054abf4dda38191600
alex2308 <alexandre.carlier@student.uclouvain.be>
2018-08-08 16:26:09 +0200

    added timer parameter fro datastreamer

--- src/datastreamer.erl
+++ src/datastreamer.erl
@@ -1,13 +1,13 @@
 -module(datastreamer).
 
--export([send_data/1,datastream/1,start_datastreamer/1]).
+-export([send_data/2,datastream/2,start_datastreamer/2]).
 
-start_datastreamer(Server) ->
+start_datastreamer(Server,Timer) ->
   Pid = spawn(datastreamer,send_data,[Server]),
   Output = register(datastream,Pid),
   io:format("Registered process as datastream is ~p ~n",[Output]).
 
-send_data(Server) ->
+send_data(Server,Timer) ->
   Dest = {connector,Server},
   Msg = {node(),connect},
   Dest ! Msg,
@@ -15,12 +15,12 @@ send_data(Server) ->
   receive
     {server_up} -> io:format("Connection accepted by the server")
   end,
-  datastream(Server).
+  datastream(Server,Freq).
 
-datastream(Server) ->
+datastream(Server,Timer) ->
   {pmod_nav, Pid, _Ref} = node_util:get_nav(),
   [Press, Temp] = gen_server:call(Pid, {read, alt, [press_out, temp_out], #{}}),
   Time = node_stream_worker:maybe_get_time(),
   {server,Server} ! {node(),Temp,Press,Time},
-  timer:sleep(1000),
-  datastream(Server).
+  timer:sleep(Timer,
+  datastream(Server,Timer).
